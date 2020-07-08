07f1eed476a2f328fe041ea4a20c5f2c2c86afe8
EricWang <a@a.a.a>
2020-07-08 23:41:53 +0800

    add test pattern

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ VARIABLE = 1
 OPERATION = 2
 EXPRESS = 3
 
+
 class Token:
     def __init__(self, type):
         self.m_type = type
@@ -15,6 +16,7 @@ class Token:
         self.m_var = None
         self.m_op = None
         self.m_exp = None
+
 
 def tokenzie(codeStr):
     codeCnt = len(codeStr)
@@ -50,35 +52,36 @@ def tokenzie(codeStr):
             token = Token(OPERATION)
             token.m_var = c
             tokens.append(token)
-            curPtr+=1
+            curPtr += 1
             print('Op: ' + c)
             continue
 
-        curPtr+=1
+        curPtr += 1
 
     return tokens
 
+
 def statementzie(codeStr):
     curPtr = 0
-    statement =[]
+    statement = []
     codeCnt = len(codeStr)
     while curPtr < codeCnt:
 
         if isSpecial(codeStr[curPtr]):
-            curPtr+=1
+            curPtr += 1
             continue
 
         end = codeStr.find(";", curPtr)
         code = codeStr[curPtr:end]
-        
+
         print("=== proc: " + code)
 
         res = tokenzie(code)
         statement.append(res)
         curPtr = end+1
 
-   
     return statement
+
 
 def get_token_value(var, env):
     res = 0
@@ -86,20 +89,23 @@ def get_token_value(var, env):
         res = var.m_num
     else:
         res = get_env_value(var.m_var, env)
-    
+
     if res == None:
         print("get_token_value: error bad var")
         print(var)
 
     return res
 
+
 def execute_assign(variable, value, env):
     set_env_value(variable, value, env)
+
 
 def check_op(token):
     if token.m_type != OPERATION:
         raise "bad op"
     return token
+
 
 def find_end_quote(tokens):
     cnt = len(tokens)
@@ -111,25 +117,27 @@ def find_end_quote(tokens):
     raise "end quote not found"
 
 
-#BNF:
+# BNF:
 # primary:     "(" expressionn ")" | NUMBER | IDENTIFY | STRING
-# factor:      "-" primary | primary  
+# factor:      "-" primary | primary
 # expression:  factor { op factor }
 
 def execute_primary(tokens, env):
     token = tokens[0]
     if token.m_var == "(":
         endIdx = find_end_quote(tokens[0:])
-        res, token = execute_exp(tokens[1 : endIdx], env)
+        res, token = execute_exp(tokens[1: endIdx], env)
         return res, tokens[endIdx+1:]
     return get_token_value(token, env), tokens[1:]
+
 
 def execute_factor(tokens, env):
     token = tokens[0]
     if token.m_var == "-":
-        val, move = execute_primary(tokens[1:], env) 
+        val, move = execute_primary(tokens[1:], env)
         return (0 - val), move
     return execute_primary(tokens, env)
+
 
 def execute_exp(tokens, env):
     res, tokens = execute_factor(tokens, env)
@@ -142,11 +150,12 @@ def execute_exp(tokens, env):
         val, tokens = execute_factor(tokens[1:], env)
         if op.m_var == "+":
             res = execute_add(res, val)
-           
+
         if op.m_var == "-":
             res = execute_sub(res, val)
 
     return res, tokens
+
 
 def execute_stmt(tokens, env):
     if tokens[0].m_type == VARIABLE:
@@ -155,21 +164,40 @@ def execute_stmt(tokens, env):
             execute_assign(tokens[0].m_var, val, env)
             tokens = tmp
 
+
 def execute(stmts, env):
     for stmt in stmts:
         execute_stmt(stmt, env)
 
+def add_code_phase_1():
+    crlf = "\r\n"
+    codeStr = ""
+    codeStr += "add1 = 100 + 200 + 300;" + crlf
+    codeStr += "add2 = 1000 - 400 - 100;" + crlf
+    codeStr += "add3 = (1000 - 200) + 100;" + crlf
+    codeStr += "add4 = 1000 - (200 + 100);" + crlf
+    codeStr += "add5 = 1000 - (200 - 100);" + crlf
+    return codeStr
+
+def test_phase_1(env):
+    if env["add1"] != 600:
+        print("error in add1 = ", env["add1"])
+    if env["add2"] != 500:
+        print("error in add2 = ", env["add2"])
+    if env["add3"] != 900:
+        print("error in add3 = ", env["add3"])
+    if env["add4"] != 700:
+        print("error in add4 = ", env["add4"])
+    if env["add5"] != 900:
+        print("error in add5 = ", env["add5"])
+
 
 crlf = "\r\n"
 codeStr = crlf
-codeStr += "aaa = 100 + 200 + 300;" + crlf
-codeStr += "bbb = 1000 - 400 - 100;" + crlf
-codeStr += "ccc = (1000 - 200) + 100;" + crlf
-codeStr += "ddd = 1000 - (200 + 100);" + crlf
-codeStr += "eee = 1000 - (200 - 100);" + crlf
+codeStr += add_code_phase_1()
+
 
 #codeStr += "eee = aaa + bbb;" + crlf
-
 
 
 print(codeStr)
@@ -181,13 +209,4 @@ execute(stmts, env)
 
 print(env)
 
-if env["aaa"] != 600:
-    print("error in aaa = ", env["aaa"])
-if env["bbb"] != 500:
-    print("error in bbb = ", env["bbb"])
-if env["ccc"] != 900:
-     print("error in ccc = ", env["ccc"])
-if env["ddd"] != 700:
-     print("error in ddd = ", env["ddd"])
-if env["eee"] != 900:
-     print("error in eee = ", env["eee"])
+test_phase_1(env)
