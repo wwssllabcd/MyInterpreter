0244c9cf13c61148ea7bc033bb44930558ff212d
EricWang <wwssllabcd@gmail.com>
2025-08-30 22:35:08 +0800

    add more test

--- TestCode.py
+++ TestCode.py
@@ -17,9 +17,74 @@ def code_add():
     res.append(["add4" , 400])
     res.append(["add5" , 500])
     res.append(["add6" , 600])
+    return codeStr, res
+
+def code_multi():
+    crlf = "\r\n"
+    codeStr = crlf
+    res = []
+    
+    codeStr += "multi_01 = 2 * 3;" + crlf
+    res.append(["multi_01" , 6])
+
+    # 3元
+    codeStr += "multi_02 = 2 * 3 + 4;" + crlf
+    res.append(["multi_02" , 10])
+
+    codeStr += "multi_03 = 2 + 3 * 4;" + crlf
+    res.append(["multi_03" , 14])
+
+    codeStr += "multi_04 = (2 + 3) * 4;" + crlf
+    res.append(["multi_04" , 20])
+
+    codeStr += "multi_05 = (2 + (3 * 4));" + crlf
+    res.append(["multi_05" , 14])
+
+    # 4元
+    codeStr += "multi_06 = 2 + 3 * 4 + 5;" + crlf
+    res.append(["multi_06" , 19])
+
+    codeStr += "multi_07 = (2 + 3) * 4 + 5;" + crlf
+    res.append(["multi_07" , 25])
+
+    codeStr += "multi_08 = 2 + (3 * 4) + 5;" + crlf
+    res.append(["multi_08" , 19])
+
+    codeStr += "multi_09 = 2 + 3 * (4 + 5);" + crlf
+    res.append(["multi_09" , 29])
 
 
     return codeStr, res
+
+
+def code_if_condition():
+    crlf = "\r\n"
+    codeStr = crlf
+    res = []
+
+    codeStr += "condi = 10;" + crlf
+
+    codeStr += "if (condi < 100) { cond_stmts_res_1 = 0; } else { cond_stmts_res_1 = 1 }" + crlf
+    res.append(["cond_stmts_res_1" , 0])
+    
+    codeStr += "if (condi > 100) { cond_stmts_res_2 = 0; } else { cond_stmts_res_2 = 1 }" + crlf
+    res.append(["cond_stmts_res_2" , 1])
+
+
+    codeStr += "if (condi < 10) { cond_stmts_res_3 = 0; } else { cond_stmts_res_3 = 1 }" + crlf
+    res.append(["cond_stmts_res_3" , 1])
+    
+    codeStr += "if (condi > 10) { cond_stmts_res_4 = 0; } else { cond_stmts_res_4 = 1 }" + crlf
+    res.append(["cond_stmts_res_4" , 1])
+
+
+    #check "else if"
+    codeStr += "if (condi > 100) { cond_stmts_res_5 = 20 + 10*2; } else if (condi < 100) { cond_stmts_res_5 = 33; } else { cond_stmts_res_5 = 30/5 - 1; }" + crlf
+    res.append(["cond_stmts_res_5" , 33])
+   
+
+    return codeStr, res
+
 
 def check_answers(answers, env):
     print("check_answers start")
@@ -33,52 +98,3 @@ def check_answers(answers, env):
             raise
 
     print("check_answers finish")
-
-def code_multi():
-    crlf = "\r\n"
-    codeStr = crlf
-    res = []
-    
-    codeStr += "multi_01 = 2 * 3;" + crlf
-    res.append(["multi_01" , 6])
-
-    codeStr += "multi_02 = 2 * 3 + 4;" + crlf
-    res.append(["multi_02" , 10])
-
-    codeStr += "multi_03 = 2 + 3 * 4;" + crlf
-    res.append(["multi_03" , 14])
-
-    codeStr += "multi_04 = (2 + 3) * 4;" + crlf
-    res.append(["multi_04" , 20])
-
-    codeStr += "multi_05 = (2 + (3 * 4));" + crlf
-    res.append(["multi_05" , 14])
-
-    codeStr += "multi_06 = (2 + 3) * (4 + 5);" + crlf
-    res.append(["multi_06" , 45])
-
-    codeStr += "multi_07 = 2 + (3 * (4 + 6)) - 2;" + crlf
-    res.append(["multi_07" , 30])
-
-    return codeStr, res
-
-
-def code_if_condition():
-    crlf = "\r\n"
-    codeStr = crlf
-    res = []
-
-    codeStr += "condi = 10;" + crlf
-
-    codeStr += "if (condi < 100) { cond_stmts_res_1 = 20 + 10*2; } else { cond_stmts_res_1 = 30/5 - 1; }" + crlf
-    res.append(["cond_stmts_res_1" , 40])
-    
-    codeStr += "if (condi > 100) { cond_stmts_res_2 = 20 + 10*2; } else { cond_stmts_res_2 = 30/5 - 1; }" + crlf
-    res.append(["cond_stmts_res_2" , 5])
-
-    #check "else if"
-    codeStr += "if (condi > 100) { cond_stmts_res_3 = 20 + 10*2; } else if (condi < 100) { cond_stmts_res_3 = 33; } else { cond_stmts_res_3 = 30/5 - 1; }" + crlf
-    res.append(["cond_stmts_res_3" , 33])
-   
-
-    return codeStr, res
